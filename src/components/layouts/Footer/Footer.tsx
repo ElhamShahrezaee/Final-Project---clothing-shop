cb6d0020de8016285f5b14e0634fb9ac4875641d
ElhamShahrezaee <eli.shahrezaee1998@gmail.com>
2026-04-29 13:47:54 +0330

    fix: update footer brand name

--- src/components/layouts/Footer/Footer.tsx
+++ src/components/layouts/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="border-t mt-10 py-6 text-center text-xs text-gray-500">
-      © {new Date().getFullYear()} Z-STORE - All rights reserved
+      © {new Date().getFullYear()} ELISHA-STOR - All rights reserved
     </footer>
   );
 };
